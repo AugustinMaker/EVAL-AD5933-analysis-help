0ebb8d8e31bc80cd0f09e9aa753947b112c4f52d
AugustinMaker <93942482+AugustinMaker@users.noreply.github.com>
2024-05-27 15:20:35 +0200

    Add files via upload

--- mosaique.py
+++ mosaique.py
@@ -38,62 +38,49 @@ def mosaique():
         nameFP = name + 'FP'
         graph(data_dict, nameFP, name)
 
+        for file in list_file:
+            file_path = 'format_data/'+ file
+            freqReal = select_data(file_path, 0, 3)
+            #print(freqPhase)
+            data_dict[file] = freqReal
+        nameFR = name + 'FR'
+        graph(data_dict, nameFR, name)
 
+        for file in list_file:
+            file_path = 'format_data/'+ file
+            freqImm = select_data(file_path, 0, 4)
+            #print(freqPhase)
+            data_dict[file] = freqImm
+        nameFIm = name + 'FIm'
+        graph(data_dict, nameFIm, name)
 
+        for file in list_file:
+            file_path = 'format_data/'+ file
+            freqPhase = select_data(file_path, 0, 2)
+            #print(freqPhase)
+            data_dict[file] = freqPhase
+        nameFP = name + 'FP'
+        graph(data_dict, nameFP, name)
 
+    if x == '2' :
+        name = input('Enter analysis name ')
+        data_dict = {}
+        print('______________________________')
+        print('0 - Frequency')
+        print('1 - Impedance')
+        print('2 - Phase')
+        print('3 - Real')
+        print('4 - Imaginary')
+        print('5 - Magnitude')
+        print('______________________________')
+        x1 = input('Enter abscisse ')
+        x2 = input('Enter ordonne ')
+        # Remplir le dictionnaire avec les données de select_data
+        for file in list_file:
+            file_path = 'format_data/'+ file
+            freqImp = select_data(file_path, int(x1), int(x2))
+            #print(freqImp)
+            data_dict[file] = freqImp
+        nameFI = name + 'FI'
+        graph(data_dict, nameFI, name)
 
-
-
-
-'''
-def process_csv(file_name):
-    csv_file = file_name + '.csv'
-    data = []
-
-    with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter=';')
-        next(reader)  # Ignorer la première ligne contenant les titres des colonnes
-
-        for row in reader:
-            try:
-                frequency = float(row[0].strip().replace(',', '.'))
-                impedance = float(row[1].strip().replace(',', '.'))
-                data.append([frequency, impedance])
-            except ValueError as e:
-                print(f"Erreur de conversion sur la ligne : {row}, erreur: {e}")
-
-    return data
-
-def plot_graphs():
-    while True:
-        try:
-            num_files = int(input("Entrez le nombre de fichiers à analyser : "))
-            if num_files <= 0:
-                raise ValueError("Le nombre de fichiers doit être positif.")
-            break
-        except ValueError as e:
-            print(f"Entrée invalide: {e}. Veuillez entrer un nombre entier positif.")
-
-    file_names = [input(f"Entrez le nom du fichier {i + 1} : ") for i in range(num_files)]
-
-    fig, axes = plt.subplots(1, num_files, figsize=(num_files * 6, 6), dpi=200)
-
-    if num_files == 1:
-        axes = [axes]
-
-    for ax, file_name in zip(axes, file_names):
-        data = process_csv(file_name)
-        frequencies = [row[0] for row in data]
-        impedances = [row[1] for row in data]
-
-        ax.plot(frequencies, impedances, label='Impedance')
-        ax.set_title(file_name)
-        ax.set_xlabel('Frequency')
-        ax.set_ylabel('Impedance')
-        ax.legend()
-        ax.grid(True)
-
-    plt.tight_layout()
-    plt.savefig('graphs_mosaic.png', bbox_inches='tight')
-
-'''
